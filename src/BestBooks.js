b18a8c8c8685e56f17ad06e8646bc331ec8f477c
Jason Christopher <jchristopher2448@gmail.com>
2022-11-23 16:47:20 -0600

    added styling

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,10 +1,7 @@
-/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
 import axios from 'axios';
 import BookFormModal from './BookFormModal';
 import { Button, Container, Carousel} from 'react-bootstrap';
-
-// let server = process.env.REACT_APP_SERVER;
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -17,7 +14,6 @@ class BestBooks extends React.Component {
     }
   }
 
-  /* TODO: Make a GET request to your API to fetch all the books from the database  */
   getBooks = async () => {
     try{
       let results = await axios.get(`${process.env.REACT_APP_SERVER}/books`);
@@ -123,11 +119,6 @@ class BestBooks extends React.Component {
     this.updateBook(Book);
   }
 
-  // showModal = (inputMode, bookID) => {
-  //   let newMode = inputMode;
-  //   this.setState({isModalShown: true, mode: newMode,})
-  // }
-
   handleCloseModal = () => {
     this.setState({
       isModalShown: false,
@@ -140,8 +131,6 @@ class BestBooks extends React.Component {
 
   render() {
 
-    /* TODO: render all the books in a Carousel */
-    // Carousel constructor
     let carouselItems = this.state.books.map(book => {
       return (
         <Carousel.Item key={book._id}>
@@ -170,23 +159,22 @@ class BestBooks extends React.Component {
             </section>
           </Carousel.Caption>
         </Carousel.Item>
-        // <Book 
-        //   book={book} 
-        //   key={book._id}
-        //   deleteBook={this.deleteBook}
-        // />
       )
     });
 
     return (
       <>
         <div id="libraryDiv">
-          <img id="libraryPic" src="https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="Photo by Susan Q Yin on Unsplash" />
-          <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+          <section 
+            id="libraryPic" 
+            // src="https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" 
+            // alt="Photo by Susan Q Yin on Unsplash" 
+          />
+          <h2>My Library</h2>
           <Button onClick={() => this.setState({isModalShown: true, mode: 'add'})}id="addBookButton">Add Book</Button>
         </div>
         {this.state.books.length ? (
-          <Container>
+          <Container id="main">
             <Carousel className="carousel">
               {carouselItems}
             </Carousel>
@@ -206,32 +194,5 @@ class BestBooks extends React.Component {
     )
   }
 }
-
-// class Book extends React.Component {
-//   render () {
-//     return (
-//       <>
-//         <Carousel.Item 
-//           key={this.props.key}
-//         >
-//           <img
-//             className="d-block w-100"
-//             src="https://via.placeholder.com/500"
-//             alt="Books"
-//             width="500"
-//             height="100"
-//           />
-//           <Carousel.Caption>
-//             <p>{this.props.book.title}</p>
-//             <p>{this.props.book.description}</p>
-//             <p>{this.props.book.status}</p>
-//             <Button onClick={() => this.props.deleteBook(this.props.book._id)}>Remove Book</Button>
-//           </Carousel.Caption>
-//         </Carousel.Item>
-//       </>
-//     );
-
-//   }
-// }
 
 export default BestBooks;
